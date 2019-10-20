4c19444b5bc7ecc352bad3d622a354a6aa8d184f
acentelles <centelles.alberto@gmail.com>
2019-10-20 13:04:32 +0100

    Test Newton polynomial interpolation

--- src/Shamir/FFT.hs
+++ src/Shamir/FFT.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE ParallelListComp #-}
 module Shamir.FFT
   ( -- shareSecret
   -- , reconstructSecret
@@ -10,14 +12,22 @@ module Shamir.FFT
   , fft3
   , inverseDft2
   , inverseDft3
+  , log2
+  , log3
+  , closestToPow2
+  , closestToPow3
+  , newtonInterpolation
+  , newtonEvaluate
+  , NewtonPolynomial(..)
   ) where
 
-import Protolude
+import Protolude hiding (quot)
 import Control.Error.Operator (assertM)
 import Control.Monad.Random (MonadRandom)
+import Data.Euclidean (quot)
 import Data.Field.Galois (PrimeField(..), rnd, GaloisField, pow, char)
 import qualified Data.List as List
-import Data.Poly (VPoly, toPoly, eval)
+import Data.Poly (VPoly, toPoly, eval, scale, deriv)
 import qualified Data.Vector as V
 
 -- | Polynomial represented as a coefficient vector, little-endian
@@ -28,8 +38,9 @@ type CoeffVec f = [f]
 -- these lists will be a power of two.)
 type DFT f = [f]
 
-generateParameters :: Int -> Integer -> Integer -> (Integer, Integer, Integer)
-generateParameters = notImplemented
+--------------------
+-- Roots of unity --
+--------------------
 
 -- | Calculate roots of unity of powers of 2
 getRootOfUnity2 :: forall f. PrimeField f => Int -> f
@@ -49,10 +60,14 @@ findNthRootOfUnity getRootOfUnity n = go 2
     go k = let root = getRootOfUnity k
            in if root `pow` n == 1 then root else go (k + 1)
 
+---------
+-- FFT --
+---------
+
 -- | Fast Fourier transformation.
 fft2
   :: GaloisField k
-  => k          -- ^ function that gives for input n the principal (3^n)-th root of unity
+  => k          -- ^ function that gives for input n the principal (2^n)-th root of unity
   -> CoeffVec k -- ^ length should be n
   -> DFT k
 fft2 omega as
@@ -60,7 +75,7 @@ fft2 omega as
       1 -> as
       n -> snd <$> List.sort combineResults
         where
-          (bsCoeffs, csCoeffs) = split as
+          (bsCoeffs, csCoeffs) = split2 as
           lHalf = length bsCoeffs
           omegaSquared = omega `pow` 2
           bsValues = fft2 omegaSquared bsCoeffs
@@ -101,33 +116,54 @@ fft3 omega as
 
 -- | Split a list into a list containing the odd-numbered and one with
 -- the even-numbered elements.
-split :: [a] -> ([a],[a])
-split = foldr (\a (r1, r2) -> (a : r2, r1)) ([], [])
+split2 :: [a] -> ([a],[a])
+split2 = foldr (\a (r1, r2) -> (a : r2, r1)) ([], [])
 
 split3 :: [a] -> ([a], [a], [a])
 split3 ls = foldr (\(i, ai) (bsi, csi, dsi)
                    -> case i `mod` 3 of
-                        1 -> (ai : bsi, csi, dsi)
-                        2 -> (bsi, ai : csi, dsi)
-                        0 -> (bsi, csi, ai : dsi)
-                  ) ([], [], []) (zip [1..] ls)
+                        0 -> (ai : bsi, csi, dsi)
+                        1 -> (bsi, ai : csi, dsi)
+                        2 -> (bsi, csi, ai : dsi)
+                  ) ([], [], []) (zip [0..] ls)
 
 -- | Append minimal amount of zeroes until the list has a length which
 -- is a power of two.
-padToNearestPowerOfTwo :: Num f => [f] -> [f]
-padToNearestPowerOfTwo [] = []
-padToNearestPowerOfTwo xs = padToNearestPowerOfTwoOf (length xs) xs
-
--- | Given n, append zeroes until the list has length 2^n.
-padToNearestPowerOfTwoOf
-  :: Num f
-  => Int -- ^ n
-  -> [f] -- ^ list which should have length <= 2^n
-  -> [f] -- ^ list which will have length 2^n
-padToNearestPowerOfTwoOf i xs = xs ++ replicate padLength 0
+padToNearestPow2 :: forall f. Num f => [f] -> [f]
+padToNearestPow2 [] = []
+padToNearestPow2 xs = padToNearestPow2Of (length xs) xs
   where
-    padLength = nearestPowerOfTwo - length xs
-    nearestPowerOfTwo = bit $ log2 i
+    -- | Given n, append zeroes until the list has length 2^n.
+    padToNearestPow2Of
+      :: Num f
+      => Int -- ^ n
+      -> [f] -- ^ list which should have length <= 2^n
+      -> [f] -- ^ list which will have length 2^n
+    padToNearestPow2Of i xs = xs ++ replicate padLength 0
+      where
+        padLength = nearestPow2 - length xs
+        nearestPow2 = bit $ log2 i
+
+padToNearestPow3 :: forall f. Num f => [f] -> [f]
+padToNearestPow3 [] = []
+padToNearestPow3 xs = padToNearestPow3Of (length xs) xs
+  where
+    -- | Given n, append zeroes until the list has length 3^n.
+    padToNearestPow3Of
+      :: Num f
+      => Int -- ^ n
+      -> [f] -- ^ list which should have length <= 3^n
+      -> [f] -- ^ list which will have length 3^n
+    padToNearestPow3Of i xs = xs ++ replicate padLength 0
+      where
+        padLength = nearestPow3 - length xs
+        nearestPow3 = 3 ^ (log3 i)
+
+closestToPow2 :: Int -> Int
+closestToPow2 = ((^) 2) . log2
+
+closestToPow3 :: Int -> Int
+closestToPow3 = ((^) 3) . log3
 
 -- | Calculate ceiling of log base 2 of an integer.
 log2 :: Int -> Int
@@ -138,24 +174,29 @@ log2 x = floorLog + correction
                  then 1
                  else 0
 
+-- | Calculate ceiling of log base 3 of an integer.
 log3 :: Int -> Int
-log3 = floor . logBase 3.0 . fromIntegral
+log3 = ceiling . logBase 3.0 . fromIntegral
 
 -- | Inverse discrete Fourier transformation, uses FFT.
 inverseDft2 :: GaloisField k => k -> DFT k -> CoeffVec k
-inverseDft2 primRootsUnity dft
+inverseDft2 primRootsUnity (padToNearestPow2 -> dft)
   = let n = fromIntegral . length $ dft
     in (/ n) <$> fft2 (recip primRootsUnity) dft
 
 -- | Inverse discrete Fourier transformation, uses FFT.
 inverseDft3 :: GaloisField k => k -> DFT k -> CoeffVec k
-inverseDft3 primRootsUnity dft
+inverseDft3 primRootsUnity (padToNearestPow3 -> dft)
   = let n = fromIntegral . length $ dft
     in (/ n) <$> fft3 (recip primRootsUnity) dft
 
 -- | Create a polynomial that goes through the given values.
-interpolate :: GaloisField k => k -> [k] -> VPoly k
-interpolate primRoots pts = toPoly . V.fromList $ inverseDft2 primRoots (padToNearestPowerOfTwo pts)
+interpolate2 :: GaloisField k => k -> [k] -> VPoly k
+interpolate2 primRoots pts = toPoly . V.fromList $ inverseDft2 primRoots pts
+
+-- | Create a polynomial that goes through the given values.
+interpolate3 :: GaloisField k => k -> [k] -> VPoly k
+interpolate3 primRoots pts = toPoly . V.fromList $ inverseDft3 primRoots pts
 
 -- | Packed secrets
 shareSecrets
@@ -176,7 +217,6 @@ shareSecrets omega2 omega3 secrets t n
       -- Extend polynomial
       let extendedPoly = poly ++ replicate (orderLarge - orderSmall) 0
       assertM (length extendedPoly == orderLarge) "Invalid number of large values"
-
       -- Evaluate polynomial to generate shares
       let shares = fft3 omega3 extendedPoly
       assertM (List.head shares == 0) "The first element of shares is not 0"
@@ -184,8 +224,8 @@ shareSecrets omega2 omega3 secrets t n
       pure $ drop 1 shares
         where
           k = length secrets
-          orderSmall = t + k + 1
-          orderLarge = n + 1
+          orderSmall = closestToPow2 (t + k + 1)
+          orderLarge = closestToPow3 (n + 1)
 
 samplePolynomial :: (MonadRandom m, PrimeField f) => f -> [f] -> Int -> m [f]
 samplePolynomial omega2 secrets t = do
@@ -194,21 +234,51 @@ samplePolynomial omega2 secrets t = do
   -- Run backward FFT to recover polynomial in coefficient representation
   pure $ inverseDft2 omega2 values
 
+-- reconstructSecrets
+--   :: PrimeField f
+--   => f
+--   -> f
+--   -> [f]            -- Shares
+--   -> Int            -- Number of secrets packed
+--   -> [f]
+-- reconstructSecrets omega2 omega3 [] _ = []
+-- reconstructSecrets omega2 omega3 shares k = traceShow smallValues secrets
+--   where
+--     largeValues = 0 : shares
+--     largeCoeffs = inverseDft3 omega3 largeValues
+--     smallCoeffs = take (length shares) largeCoeffs
+--     smallValues = fft2 omega2 (padToNearestPow2 smallCoeffs)
+--     secrets = take k (drop 1 smallValues)
+
 reconstructSecrets
-  :: PrimeField f
+  :: forall f. PrimeField f
   => f
   -> f
   -> [f]            -- Shares
   -> Int            -- Number of secrets packed
   -> [f]
 reconstructSecrets omega2 omega3 [] _ = []
-reconstructSecrets omega2 omega3 shares k = secrets
+reconstructSecrets omega2 omega3 shares k
+  = let points = 1 : ((omega3 `pow`) <$> [1..length shares])
+        values = 0 : shares
+        poly = newtonInterpolation points values
+          -- interpolate2 omega3 values
+          -- lagrangeInterpolate points values
+    in traceShow poly $ take k ((newtonEvaluate poly  . (omega2 `pow`)) <$> [1..])
+       -- take k ((eval poly  . (omega2 `pow`)) <$> [1..])
   where
-    largeValues = 0 : shares
-    largeCoeffs = inverseDft3 omega3 largeValues
-    smallCoeffs = take (length shares) largeCoeffs
-    smallValues = fft2 omega2 smallCoeffs
-    secrets = take k (drop 1 smallValues)
+    lagrangeInterpolate :: [f] -> [f] -> VPoly f
+    lagrangeInterpolate xs ys = traceShow phis sum
+      [ scale 0 f (roots `quot` (root x))
+      | f <- zipWith (/) ys phis
+      | x <- xs
+      ]
+      where
+        phis :: [f]
+        phis = map (eval (deriv roots)) xs
+        roots :: VPoly f
+        roots = foldl' (\acc xi -> acc * (root xi)) 1 xs   -- (X - x_0) * ... * (X - x_{n-1})
+        root xi = toPoly . V.fromList $ [-xi,  1]          -- (X - x_i)
 
 -- -- | Create shares from a secret
 -- -- See https://mortendahl.github.io/2017/06/24/secret-sharing-part2/
@@ -225,3 +295,49 @@ reconstructSecrets omega2 omega3 shares k = secrets
 -- reconstructSecret :: PrimeField f => [f] -> f
 -- reconstructSecret [] = 0
 -- reconstructSecret shares = eval (interpolate getRootOfUnity shares) 0
+
+data NewtonPolynomial f = NewtonPolynomial
+  { npPoints :: [f]
+  , npCoeffs :: [f]
+  } deriving (Show, Eq)
+
+-- (3, 4, 3)
+-- (2, 3, 9)
+-- (1, 2, -12)
+-- (0, 1, 8)
+-- (2, 4, -3)
+-- (1, 3, 2)
+-- (0, 2, -10)
+-- (1, 4, -13)
+-- (0, 3, 4)
+-- (0, 4, 0)
+
+newtonInterpolation :: PrimeField f => [f] -> [f] -> NewtonPolynomial f
+newtonInterpolation points values = NewtonPolynomial points coeffs
+  where
+    initialStore = List.zip3 [0..] [0..] values
+    coeffs = (\(_, _, c) -> c) <$> store
+    g accStore i = take i accStore ++ ((indexLower, indexUpper, frac) : drop (i+1) accStore)
+      where
+        prevStoreElem = accStore List.!! (i-1)
+        currStoreElem = accStore List.!! i
+        (indexLower, _, coefLower) = prevStoreElem
+        (_, indexUpper, coefUpper) = currStoreElem
+        pointLower = points List.!! indexLower
+        pointUpper = points List.!! indexUpper
+        pointDiff = pointUpper - pointLower
+        pointDiffInv = recip pointDiff
+        coefDiff = coefUpper - coefLower
+        frac = coefDiff * pointDiffInv
+    f currStore j = foldl' g currStore (reverse [j..storeLen])
+    store = foldl' f initialStore [1..storeLen]
+    storeLen = length initialStore - 1
+
+newtonEvaluate :: PrimeField f => NewtonPolynomial f -> f -> f
+newtonEvaluate NewtonPolynomial{..} point
+  = sum $ (\(p, c) -> c * p) <$> zip newtonPoints npCoeffs
+  where
+    f acc i = let diff = point - (npPoints List.!! i)
+                  product = (acc List.!! i) * diff
+              in acc ++ [product]
+    newtonPoints = foldl' f [1] [0..length npPoints - 2]
--- test/Main.hs
+++ test/Main.hs
@@ -8,8 +8,10 @@ import Protolude
 import Control.Monad.Random (getRandom)
 import Data.Field.Galois (Prime, rnd, pow, PrimeField)
 import Data.List ((!!))
+import Data.Poly (VPoly, toPoly, eval, scale, deriv, unPoly)
+import qualified Data.Vector as V
 import Shamir (shareSecret, reconstructSecret)
-import qualified Shamir.FFT as FFT
+import Shamir.FFT as FFT
 import Test.QuickCheck.Monadic (monadicIO)
 import Test.Tasty
 import Test.Tasty.HUnit
@@ -45,30 +47,35 @@ prop_shamir_lagrange secret = monadicIO $ do
 --   shares <- lift $ FFT.shareSecret secret k n
 --   pure $ and
 --     [ secret == FFT.reconstructSecret shares
---     -- TODO: Enable this. Still not work for less than n shares
---     -- , secret == FFT.reconstructSecret getRootOfUnity (take k shares)
 --     , secret /= FFT.reconstructSecret (take (k-1) shares)
 --     ]
 
-type SmallPrime = Prime 433
+type Prime433 = Prime 433
 
-test_roots_of_unity :: PrimeField f => (Int -> f) -> Int -> Property
-test_roots_of_unity getRootOfUnity n = monadicIO $ do
+prop_find_nth_root_of_unity :: PrimeField f => (Int -> f) -> Int -> Property
+prop_find_nth_root_of_unity getRootOfUnity n = monadicIO $ do
   let omega = FFT.findNthRootOfUnity getRootOfUnity n
   pure $ omega `pow` n == 1
 
 test_packed_example_1 :: Assertion
 test_packed_example_1 = do
-  let t = 4 -- threshold
+  let secrets = [1,2,3] :: [Prime433]
+      t = 3 -- threshold
       n = 8 -- #shares
-      k = 3 -- #secrets
-      omega2 = 354       -- `m`-th principal root of unity in Zp,
-                         -- where `m = secret_count + threshold + 1`
-                         -- must be a power of 2
-      omega3 = 150       -- `n`-th principal root of unity in Zp, where
-                         -- `n = share_count + 1` must be a power of 3.
-      secrets = [1,2,3] :: [SmallPrime]
+      k = length secrets -- #secrets
+      m = FFT.closestToPow2 (k + t + 1)
+      -- `m`-th principal root of unity in Zp,
+      -- where `m = secret_count + threshold + 1`
+      -- must be a power of 2
+      omega2 = FFT.findNthRootOfUnity FFT.getRootOfUnity2 m
+  traceShowM omega2
+      -- `n`-th principal root of unity in Zp, where
+      -- `n = share_count + 1` must be a power of 3
+  let n' = FFT.closestToPow3 (n + 1)
+      omega3 = FFT.findNthRootOfUnity FFT.getRootOfUnity3 n'
+  traceShowM omega3
   shares <- FFT.shareSecrets omega2 omega3 secrets t n
+  traceShowM $ ("Shares: ", shares)
   secrets @=? FFT.reconstructSecrets omega2 omega3 shares k
 
 test_fft2_example_1 :: Assertion
@@ -76,7 +83,7 @@ test_fft2_example_1 = do
   let omega2 = 354
       aCoeffs = [1, 2, 3, 4, 5, 6, 7, 8]
       aPoints = FFT.fft2 omega2 aCoeffs
-  aPoints @=? ([36, -130, -287, 3, -4, 422, 279, -311] :: [SmallPrime])
+  aPoints @=? ([36, -130, -287, 3, -4, 422, 279, -311] :: [Prime433])
   FFT.inverseDft2 omega2 aPoints @=? aCoeffs
 
 
@@ -85,10 +92,10 @@ test_fft3_example_1 = do
   let omega3 = 150
       aCoeffs = [1, 2, 3, 4, 5, 6, 7, 8, 9]
       aPoints = FFT.fft3 omega3 aCoeffs
-  aPoints @=? ([45, 404, 407, 266, 377, 47, 158, 17, 20] :: [SmallPrime])
+  aPoints @=? ([45, 404, 407, 266, 377, 47, 158, 17, 20] :: [Prime433])
   FFT.inverseDft3 omega3 aPoints @=? aCoeffs
 
-type MediumPrime = Prime 746497
+type Prime746497 = Prime 746497
 
 test_packed_example_2 :: Assertion
 test_packed_example_2 = do
@@ -100,9 +107,28 @@ test_packed_example_2 = do
                          -- must be a power of 2
       omega3 = 610121    -- `n`-th principal root of unity in Zp, where
                          -- `n = share_count + 1` must be a power of 3.
-  secrets <- replicateM 100 (rnd @MediumPrime)
+  secrets <- replicateM 100 (rnd @Prime746497)
   shares <- FFT.shareSecrets omega2 omega3 secrets t n
   secrets @=? FFT.reconstructSecrets omega2 omega3 shares k
+
+type Prime17 = Prime 17
+test_newton_interpolation_general :: Assertion
+test_newton_interpolation_general = do
+  let coeffs = [1,2,3,4] :: [Prime17]
+      poly = toPoly . V.fromList $ coeffs
+      points = [5,6,7,8,9]
+      values = eval poly <$> points
+
+  [8,16,4,13,16] @=? values
+
+  let recoveredPoly = newtonInterpolation points values
+      recoveredValues = newtonEvaluate recoveredPoly <$> points
+
+  NewtonPolynomial points [8, 8, -10, 4, 0] @=? recoveredPoly
+  values @=? recoveredValues
+  3 @=? newtonEvaluate recoveredPoly 10
+  15 @=? newtonEvaluate recoveredPoly 11
+  8 @=? newtonEvaluate recoveredPoly 12
 
 main :: IO ()
 main = defaultMain $
@@ -111,11 +137,14 @@ main = defaultMain $
     [ testCase "FFT2 small example" test_fft2_example_1
     , testCase "FFT3 small example" test_fft3_example_1
     ]
+  , testGroup "Newton Polynomial"
+    [ testCase "General" $ test_newton_interpolation_general
+    ]
   , testGroup "Roots of unity"
     [ testProperty "Find Nth root of unity. Powers of 2"
-      $ test_roots_of_unity (FFT.getRootOfUnity2 @SmallPrime)
+      $ prop_find_nth_root_of_unity (FFT.getRootOfUnity2 @Prime433)
     , testProperty "Find Nth root of unity. Powers of 3"
-      $ test_roots_of_unity (FFT.getRootOfUnity3 @SmallPrime)
+      $ prop_find_nth_root_of_unity (FFT.getRootOfUnity3 @Prime433)
     ]
   , testGroup "Shamir secret sharing"
     [ testProperty "Lagrange" prop_shamir_lagrange
